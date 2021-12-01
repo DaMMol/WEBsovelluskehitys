ae5cc852bb4bbe9b115de908fc53364eeda2c99b
Rutto404 <daniel.molnar997@gmail.com>
2021-12-01 18:05:21 +0200

    Tehtava15 Node-tehtavia

---
+++ labs/Node tehtävät/Tehtava9.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const mariadb = require('mariadb');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const pool = mariadb.createPool({
+    host:'localhost',
+    user:'olso',
+    password:'olso',
+    database:'webkanta'
+});
+
+app.get('/', function(req,res){
+    res.sendFile('Tehtava9.html', {root: __dirname});
+});
+
+app.post('/view', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT event_date.Date, event.Name, event.Type, Location.Location_name FROM event_date, event, location WHERE event_date.Event_id = event.Event_id and event.Location_Location_id = Location.Location_id and event_date.Date >= ? and event_date.Date <= ? GROUP BY Name ORDER BY event_date.Date', [req.body.aloitusdate,req.body.lopetusdate]);
+        console.log(result);
+        let lista = "";
+        for(let i = 0; i<result.length;i++) {
+            lista += "<br><br><li> Aika: " + JSON.stringify(result[i].Date)
+                + "<br> Nimi: " + JSON.stringify(result[i].Name)
+                + "<br> Tyyppi: " + JSON.stringify(result[i].Type)
+                + "<br> Paikka: " + JSON.stringify(result[i].Location_name)
+                + "</li>";
+        };
+        res.send("<div><ol>" + lista + "</ol></div>");
+    } catch (err) {
+        throw err;
+    }
+});
+
+app.post('/update', async (req, res) => {
+    try {
+        let jsonObj = req.body;
+        let responseString = JSON.stringify(jsonObj);
+        const result = await pool.query('INSERT INTO event (Name, Type, Location_Location_id) VALUES (?,?,?)', [jsonObj.eventName,jsonObj.eventType,jsonObj.eventLocation,]);
+        let insertedId = result.insertId;
+        console.log(result);
+        await pool.query('INSERT INTO Event_date (Date,Event_id) VALUES (?,?)', [jsonObj.eventDate,insertedId]);
+        res.send("POST SUCCESFUL " + responseString);
+    } catch (err) {
+        throw err;
+    }
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
